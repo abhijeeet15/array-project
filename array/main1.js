cac3aee93ec9d511d036cc170d58ba1e71bcd729
abhijeeet15 <168513610+abhijeeet15@users.noreply.github.com>
2025-05-13 14:38:52 +0530

    Add files via upload

---
+++ array/main1.js
@@ -0,0 +1,57 @@
+// <---------------------------- print the array ----------------------------->
+
+// let abhi = [18,"sutar",6731]
+
+// for (let i = 0; i < abhi.length; i++) {
+//     document.writeln(abhi[i]);
+// }
+
+// <--------------------- print only even numbers of array  ----------------------------->
+
+// let arr = [1,2,3,4,5,6]
+
+
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] % 2 === 0) {
+//       document.writeln(arr[i]);
+//     }
+//   }
+
+// <-------------------------- show products price ----------------------------->
+
+// let arr = [100,200,95,60,50]
+
+//  for(let i=0; i<arr.length; i++){
+//     if(arr[i]>=200){
+//         document.writeln(arr[i])
+//     }
+//  }
+// <--------------------------- print sum of array. ----------------------------->
+
+//  let arr = [100,300,400,500]
+//  let total = 0
+
+//  for (let i = 0; i < arr.length; i++) {
+//         total = total + arr[i];
+//     }
+
+//  document.writeln(total);
+
+// <--------------------------- printf reverse array. ----------------------------->
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+//     for (let i = arr.length - 1; i >= 0; i--) {
+//         document.writeln(arr[i]);
+//     }
+
+
+// <---------------------- Perform addition between two array. ----------------------------->
+
+
+//   let abhi = [1, 2, 3, 4, 5];
+//   let karan = [10, 20, 30, 40, 50];
+
+//     for (let i = 0; i < abhi.length; i++) {
+//         document.writeln(abhi[i] + karan[i]);
+//     }
